99c1ca6c5a0a07e968462bf205a6b3e85feab5d5
wrmk <elmakan@gmail.com>
2021-06-21 19:33:59 +0300

    create test

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,26 +1,46 @@
 class Notification < ApplicationRecord
+  validates :create_at, :sent_at, :confirmed_at, presence: true
 
-require 'faker' 
-require 'net/http'
 
-def greet(interval,count)
-  (1..count).each do |i|
+  require 'faker' 
+  require 'net/http'
 
-    message = Faker::Fantasy::Tolkien.poem
-    create_time = DateTime.now
-    delivery_time = ''
-
-    uri = URI('http://www.example.com/')
-    send_time = DateTime.now
-    res = Net::HTTP.post_form(uri, 'q' => message, 'max' => '50')
-
-    delivery_time = DateTime.now if res.code == '200'
-
-    notification=Notification.create(create_at: create_time, sent_at: send_time, confirmed_at: delivery_time)
+  def greet(interval,count)
     
-    sleep(interval) if i != count
- end 
-end
+   if !check_value(interval) or !check_value(count) then abort("Введите корректные значения интервала и количества сообщений") end
+    
+    (1..count).each do |i|
+      message = Faker::Fantasy::Tolkien.poem
+      create_time = DateTime.now
+      @send_time = ''
+      @delivery_time = ''
+
+      uri = URI('http://www.example.com/')
+      res = Net::HTTP.post_form(uri, 'q' => message, 'max' => '50')
+      
+
+      @send_time = DateTime.now
+      @delivery_time = DateTime.now if res.code == '200'
+      
+
+      notification=Notification.create(create_at: create_time, sent_at: @send_time, confirmed_at: @delivery_time)
+      
+      sleep(interval) if i != count
+      
+   end 
+  end
+
+  def check_value(x)
+    !x.nil? and x.is_a?(Integer) and x.to_i>0 ? true : false
+  end
+
+  def doesnt_delivered
+    @delivery_time == ''
+  end
+
+  def doesnt_send
+    @send_time == ''
+  end
 
 
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save nill data" do
+    message=Notification.new
+    assert_not message.save
+  end
+
+  test "message doesn`t received" do
+    message=Notification.new
+    message.greet(1,1)
+    assert_not message.doesnt_delivered
+  end
+
+  test "message doesn`t send" do
+    message=Notification.new
+    message.greet(1,1)
+    assert_not message.doesnt_send
+  end
+
+  test "validates parameters" do
+    message=Notification.new
+    assert_not message.check_value('c')
+    assert_not message.check_value(-1)
+    assert_not message.check_value('')
+  end
+
+
+
+
 end
